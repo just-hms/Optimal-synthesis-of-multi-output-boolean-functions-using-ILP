504b9883a820125d3266b618706c085d465f8461
just-Hms <alessandroversari1@gmail.com>
2022-05-05 18:41:13 +0200

    fix: unique is now used to generate Zs

--- src/multipleOutputSynthesis.m
+++ src/multipleOutputSynthesis.m
@@ -1,26 +1,29 @@
-function [implicants, v] = multipleOutputSynthesis(variables_number, outputs, diodes_cost)
+function [implicants, v] = multipleOutputSynthesis(inputsNumber, outputs, options)
 
-    % TODO maybe use repeating arguments for the outputs
     arguments
-        variables_number (1,1) double {mustBeInteger, mustBePositive}
-        outputs (:,2) cell 
-        diodes_cost (1,1) double {mustBeNumericOrLogical} = 1
-    end
+        inputsNumber (1,1) double {mustBeInteger, mustBePositive}
+        outputs (:,2) cell
 
+        options.DiodesCost (1,1) double {mustBeNumericOrLogical} = 1
+        options.Verbose (1,1) double {mustBeNumericOrLogical} = 0
+    end
+    
     % returns
     %   the minimal cost synthesis of the boolean outputs described by them
     
-    implicants_set = [];
-    implicants_counts = zeros(length(outputs), 1);
+    % set of all implicants
+    implicantsSet = [];
+    outputsImplicantsCount = zeros(length(outputs), 1);
+    
     A = [];
-    minterms_len = 0;
+    totalMintermLength = 0;
 
-    not_redundant_implicants = [];
+    notRedundantImplicants = [];
 
-    for i = 1:length(outputs) ; f = outputs(i); 
+    for i = 1:length(outputs) ; output = outputs(i); 
 
-        minterms = f{1}{1}; 
-        dont_cares = f{1}{2};
+        minterms = output{1}{1}; 
+        dont_cares = output{1}{2};
         
         if ~ utils.isInAscendingOrder(minterms)
             error('Minterms must be in ascending order')
@@ -30,83 +33,107 @@ function [implicants, v] = multipleOutputSynthesis(variables_number, outputs, di
             error('Dont_cares must be in ascending order')
         end
         
-        [implicants_k, A_k] = getAllImplicants(variables_number, minterms, dont_cares);
+        [implicants_k, A_k] = getAllImplicants(inputsNumber, minterms, dont_cares);
         
-        implicants_counts(i) = length(implicants_k);
-        implicants_set = [implicants_set ; implicants_k];
+        outputsImplicantsCount(i) = length(implicants_k);
+        implicantsSet = [implicantsSet ; implicants_k];
         
+        % every coverage matrix' costraints are indipendent
         A = blkdiag(A, A_k);
 
-        minterms_len = minterms_len + length(minterms);
+        totalMintermLength = totalMintermLength + length(minterms);
         
-    end
-
-    % create the not redundant list of implicants_set
-    for i = 1:length(implicants_set) ; implicant = implicants_set(i);
-        if ~ any(strcmp(not_redundant_implicants, implicant))                
-            not_redundant_implicants = [not_redundant_implicants ; implicant];
+        if options.Verbose
+            fprintf('\nOutput %d:\n\n',i);
+            fprintf('All possible implicants are:\n\n') ; disp(implicants_k)
+            fprintf('The coverage matrix is:\n\n')      ; disp(A_k)
         end
+
     end
-    
-    E = eye(length(not_redundant_implicants));
+
+    notRedundantImplicants = unique(implicantsSet);
+
+    % one more costraint for every notRedundantImplicant
+    %   every notRedundantImplicants must be chosen if a corresponding implicant is chosen
+
+    E = eye(length(notRedundantImplicants));
     A = blkdiag(A, E);
-
-    % choice costraints 
-    for i = 1:length(not_redundant_implicants); implicant = not_redundant_implicants(i);
-        A(minterms_len + i, implicants_set == implicant) = -1 / (length(outputs) + 1);
-    end
-
-    total_implicants_len = length(implicants_set) + length(not_redundant_implicants); 
-
-    % ports cost
-    C = ones(total_implicants_len, 1);
-    C(1:length(implicants_set)) = 0;
     
-    b = zeros(minterms_len + length(not_redundant_implicants), 1);
-    b(1:minterms_len) = 1;
+    % choice costraints:
+    %   Z_i > ∑ V_ij * 1/(length(outputs) + 1)
+
+    for i = 1:length(notRedundantImplicants); notRedundantImplicant = notRedundantImplicants(i);
+        A(totalMintermLength + i, implicantsSet == notRedundantImplicant) = -1 / (length(outputs) + 1);
+    end
+    
+    variablesLength = length(implicantsSet) + length(notRedundantImplicants); 
+    
+    % ports cost
+    % only the notRedundantImplicants must be considered in the cost
+    C = ones(variablesLength, 1);
+    C(1:length(implicantsSet)) = 0;
+    
+    % in the choice costraints the costant value is 0 
+    % in the cover constraints the costant value is 1
+    b = zeros(totalMintermLength + length(notRedundantImplicants), 1);
+    b(1:totalMintermLength) = 1;
     
     % diodes cost
-    if diodes_cost
-        for i = 1:length(implicants_set)
+    if options.DiodesCost
+
+        % 1 every time a port is used in an output
+        for i = 1:length(implicantsSet)
             C(i) = 1; 
         end
 
-        for i = 1:length(not_redundant_implicants)
-            C(i + length(implicants_set)) =  utils.countMatches(not_redundant_implicants(i, :), "0" | "1");
+        % c_i every time a port is chosen
+        for i = 1:length(notRedundantImplicants)
+            C(i + length(implicantsSet)) =  utils.countMatches(notRedundantImplicants(i, :), "0" | "1");
         end
     end
 
-    % all variables must be integers
-    intcon = 1:total_implicants_len;
-    
-    % lower and upper bounds are 0s and 1s
-    lb = zeros(total_implicants_len, 1);
-    ub = ones(total_implicants_len, 1);
-    
-    [x, v] = intlinprog(C,intcon,- A, - b,[],[],lb,ub);
-
-    % or port cost
-    if ~ diodes_cost
-        v = v + 1;
+    if options.Verbose
+        fprintf('The not redundant implicants are:\n\n')    ; disp(notRedundantImplicants)
+        fprintf('The choice matrix is:\n\n')                ; disp(A(totalMintermLength + 1:end, :))
+        fprintf('The cost vector is:\n\n')                  ; disp(C)
     end
 
-    % build the result implicants (one array for every output)
-    implicants = cell(length(outputs), 1);
-    current_count = 1;
-    j = 1;
+    [x, v] = utils.intlinprogWrapper(C, -A, -b, variablesLength, options.Verbose);
 
-    for i = 1:length(implicants_set)
+    % one OR for output
+    if ~ options.DiodesCost
+        v = v + length(output);
+    end
+
+    if options.Verbose
+        fprintf('Solution:\n\n')    ; disp(x)
+        fprintf('The cost is:\n\n') ; disp(v)
+    end
+
+    % build the result implicants
+
+    implicants = cell(length(outputs), 1);
+    
+    outputIndex = 1;
+    outputImplicantsIndex = 1;
+    
+    % cycle through every implicant
+    for i = 1:length(implicantsSet)
         
+        % if the ith implicant is has been chosen add it
         if x(i)
             
-            if current_count > implicants_counts(j)
-                j = j + 1;
+            % if the outputImplicantsIndex overflow got to the next output
+            if outputImplicantsIndex > outputsImplicantsCount(outputIndex)
+                outputImplicantsIndex = 1;
+                outputIndex = outputIndex + 1;
             end
 
-            implicants{j} = [implicants{j} ; implicants_set(i)];     
+            implicants{outputIndex} = [implicants{outputIndex} ; implicantsSet(i)];     
         end
         
-        current_count = current_count + 1;
+        % increment the current outputImplicantsIndex 
+        outputImplicantsIndex = outputImplicantsIndex + 1;
 
     end 
 
